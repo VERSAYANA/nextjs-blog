696100f96b31d4543471955f72a37145e4f0094e
Ramtin Khalatbari <VERSAYANA@outlook.com>
2020-07-18 23:30:38 +0430

    Convert to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,15 @@ import Layout, { siteTitle } from '../components/layout'
 import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import { GetStaticProps } from 'next'
 
-
-export default function Home({ allPostsData }) {
-  console.log(allPostsData)
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    date: string,
+    title: string,
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -40,7 +45,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
